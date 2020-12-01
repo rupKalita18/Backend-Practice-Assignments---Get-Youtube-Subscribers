afe24200ec49baeb146280df827c80713b228736
rexxx18 <65245341+rexxx18@users.noreply.github.com>
2020-12-01 18:07:59 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,61 +1,40 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose=require('mongoose');
-const connection=require('./models/subscribers');
+const mongoose = require("mongoose");
+const connection = require("./models/subscribers");
 
-
-
-
-app.get('/subscribers',async(req,res)=>{
-  const subscribers=await connection.find();
+app.get("/subscribers", async (req, res) => {
+  const subscribers = await connection.find();
   res.send(subscribers);
-})
+});
 
+app.get("/subscribers/names", async (req, res) => {
+  const subscribers = await connection.find(
+    {},
+    { name: 1, subscribedChannel: 1, _id: 0 }
+  );
+  console.log(subscribers);
+  res.json(subscribers);
+});
 
-app.get('/subscribers/names',async(req,res)=>{
-    const subscribers=await connection.find({},{name:1,subscribedChannel:1,_id:0})
-    console.log(subscribers);
-   res.json(subscribers);
-})
-
-app.get('/subscribers/:id',async(req,res)=>{
-    const id=req.params.id;
-    const subscribers= await connection.find({_id:id}).catch((err)=>{
-       res.status(400).send({message: "error.message"})
-       
+app.get("/subscribers/:id", (req, res) => {
+  const id = req.params.id;
+  connection
+    .find({ _id: id }, { _id: 0, name: 1 })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.status(400).send({ message: "error message" });
     });
-    // if(subscribers===undefined)
-    // {
-    //     res.status(400).send({message: error.message});
-    //     return;
-    // }
-    // console.log(subscribers);
-    res.send(subscribers);
-})
-
-
+  // if(subscribers===undefined)
+  // {
+  //     res.status(400).send({message: error.message});
+  //     return;
+  // }
+  // console.log(subscribers);
+});
 
 // Your code goes here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = app;
