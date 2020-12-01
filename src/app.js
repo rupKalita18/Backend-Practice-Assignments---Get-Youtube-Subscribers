e3e97069a4eb1bc3bbfba3e18a1ec9d2c23ef955
rexxx18 <65245341+rexxx18@users.noreply.github.com>
2020-12-01 17:59:06 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,39 @@
 
 const express = require('express');
-const app = express()
+const app = express();
+const mongoose=require('mongoose');
+const connection=require('./models/subscribers');
+
+
+
+
+app.get('/subscribers',async(req,res)=>{
+  const subscribers=await connection.find();
+  res.send(subscribers);
+})
+
+
+app.get('/subscribers/names',async(req,res)=>{
+    const subscribers=await connection.find({},{name:1,subscribedChannel:1,_id:0})
+    console.log(subscribers);
+   res.json(subscribers);
+})
+
+app.get('/subscribers/:id',async(req,res)=>{
+    const id=req.params.id;
+    const subscribers= await connection.find({_id:id}).catch((err)=>{
+       res.status(400).send({message: "error.message"})
+       
+    });
+    // if(subscribers===undefined)
+    // {
+    //     res.status(400).send({message: error.message});
+    //     return;
+    // }
+    // console.log(subscribers);
+    res.send(subscribers);
+})
+
 
 
 // Your code goes here
